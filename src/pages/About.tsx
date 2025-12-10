345b9f44c0c536593b743d0638670e429e920d67
demo29044-a11y <demo29044@gmail.com>
2025-12-10 12:41:19 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,10 @@ import FeaturesSection from "@/components/about/FeaturesSection";
 import HowItWorksSection from "@/components/about/HowItWorksSection";
 import WhyChooseUsSection from "@/components/about/WhyChooseUsSection";
 import StorySection from "@/components/about/StorySection";
+import ValuesSection from "@/components/about/ValuesSection";
+import TestimonialsSection from "@/components/about/TestimonialsSection";
 import TeamSection from "@/components/about/TeamSection";
+import FAQSection from "@/components/about/FAQSection";
 import CTASection from "@/components/about/CTASection";
 
 const About = () => {
@@ -22,7 +25,10 @@ const About = () => {
         <HowItWorksSection />
         <WhyChooseUsSection />
         <StorySection />
+        <ValuesSection />
+        <TestimonialsSection />
         <TeamSection />
+        <FAQSection />
         <CTASection />
       </main>
       <Footer />
